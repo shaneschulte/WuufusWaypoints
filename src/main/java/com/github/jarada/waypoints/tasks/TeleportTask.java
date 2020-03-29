f069832a8c4f23c2c819a89899e7eb9feb3937ac
Jarada <jarada0077@mac.com>
2020-03-29 13:51:29 +0100

    Adjust Plugin for 1.13, 1.14 and 1.15 support

--- src/main/java/com/github/jarada/waypoints/tasks/TeleportTask.java
+++ src/main/java/com/github/jarada/waypoints/tasks/TeleportTask.java
@@ -12,7 +12,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import org.bukkit.metadata.FixedMetadataValue;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import com.github.jarada.waypoints.PluginMain;
 import com.github.jarada.waypoints.Util;
